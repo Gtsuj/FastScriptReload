16cb38b80a1d8621685040b1fe56290e0fe76f1c
邱鹏 <qiup@g-bits.com>
2025-12-04 14:33:29 +0800

    添加字段Hook的支持

---
+++ Assets/Scripts/Editor/Base/HookFieldInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Markup;
+using Mono.Cecil;
+
+namespace FastScriptReload.Editor
+{
+    public class HookFieldInfo
+    {
+        /// <summary>
+        /// 是否被修改（dirty标记）
+        /// </summary>
+        public bool IsDirty { get; set; }
+
+        /// <summary>
+        /// 字段类型
+        /// </summary>
+        public FieldDefinition FieldType { get; set; }
+    }
+}
